1d2eb22f4bbe94c7abcc71a51f70556441a78767
Tausif <tausif00100@gmail.com>
2025-01-21 23:21:22 +0530

    profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,59 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import images from '~/constants/images';
+import icons from '../../constants/icons';
+
 const Profile = () => {
   return (
     <SafeAreaView className={nativewind.screen}>
-      {/* For images */}
-      <View className="  items-center justify-center bg-green-300">
-        <Image source={images.avatar} className="  size-40 rounded-full" />
-      </View>
+      <ScrollView>
+        {/* For images */}
+        <View className="  flex-col items-center justify-center gap-3 bg-green-300">
+          <Image source={images.avatar} className="  size-40 rounded-full" />
+          <Text>John Doe</Text>
+        </View>
+
+        {/* for horizontal line */}
+        <View className=" mt-8 border-b border-t  border-[#E6F0FF]">
+
+
+          {/* container for payments and booking */}
+          <View className=" flex-row items-center justify-between p-2">
+            <View className=" flex-row  items-center gap-2">
+              <Image source={icons.calendar} resizeMode="contain" className="  h-7 w-7 " />
+              <Text>My Booking</Text>
+            </View>
+
+            <View>
+              <Image source={icons.rightArrow} resizeMode="contain" className="  h-7 w-7 " />
+            </View>
+          </View>
+
+
+          <View className=" flex-row items-center justify-between p-2">
+            <View className=" flex-row  items-center gap-2">
+              <Image source={icons.wallet} resizeMode="contain" className="  h-7 w-7 " />
+              <Text>Payments</Text>
+            </View>
+
+            <View>
+              <Image source={icons.rightArrow} resizeMode="contain" className="  h-7 w-7 " />
+            </View>
+          </View>
+        </View>
+
+
+{/* for logout */}
+ {/* Logout */}
+ <TouchableOpacity 
+ activeOpacity={0.7}
+ className=" flex-row items-center gap-2 mt-4">
+ <Image source={icons.logout} resizeMode="contain" className="  h-7 w-7 " />
+<Text className=' text-red-500 font-semibold text-xl'>Logout</Text>
+ </TouchableOpacity>
+
+      </ScrollView>
     </SafeAreaView>
   );
 };
