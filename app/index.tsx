7bf67bc74d77d86260fc578799a376a573e16e6f
Tausif <tausif00100@gmail.com>
2025-01-22 00:03:46 +0530

    add onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,33 +2,81 @@ import { StyleSheet, Text, View , Image, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import { Redirect } from 'expo-router'
+import images from '~/constants/images'
+import icons from '~/constants/icons'
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router'
 
 const Onboarding = () => {
+  const router = useRouter()
+
+  const move = () => {
+    router.replace('/(tabs)/home')
+  }
   return (
     <SafeAreaView
-    style={styles.screen}
-    >
+    className={nativewind.screen}>
+    
+      {/* don't delete this just: after compiltion of project can be delete */}
       {/* <Text style={{fontFamily: 'Rubik-Bold'}}>RootLayout</Text>
       <Text style={{fontFamily: 'Rubik-ExtraBold'}}>RootLayout</Text>
       <Text style={{fontFamily: 'Rubik-Medium'}}>RootLayout</Text>
       <Text style={{fontFamily: 'Rubik-Regular'}}>RootLayout</Text>
       <Text style={{fontFamily: 'Rubik-SemiBold'}}>RootLayout</Text> */}
 
-      {/* <Image 
-      source={require("../assets/images/splash.png")}
-      resizeMode= 'contain'
-      style={{height: 250 , width: "100%"}}
-      /> */}
-      <Redirect href={"/(tabs)/profile"}/>
-      <Text className=' text-gray-400 '>WELCOME TO REAL SCOUT</Text>
-      <Text className='  text-primary-100'>Let's Get You Closer</Text>
-      <Text>TO <Text
-      className=' text-green-500 text-2xl  '
-      >Your Ideal Home </Text></Text>
-      <Text className='text-green-700 '>Login to Real Scout with Google</Text>
-      <TouchableOpacity>
+    
+      {/* <Redirect href={"/(tabs)/profile"}/> */}
+
+      <Image 
+      source={images.onboarding}
+      className= 'h-[60%] w-full'
+      />
+
+      {/* Texts */}
+
+<LinearGradient 
+ colors={['#ffffff', 'transparent']}
+
+>
+
+      <View
+      className=' flex-col items-center mt-5'
+      >
+
+      <Text className=' text-[#666876] '>WELCOME TO REAL SCOUT</Text>
+
+      {/* Cause it's group of welcoming text */}
+      <View
+      className=' flex-col  mt-4 mb-4'
+      >
+      <Text className=' text-2xl font-bold'>Let's Get You Closer</Text>
+      <Text className=' text-2xl font-bold'>TO <Text
+      className=' text-blue-500 text-2xl '>Your Ideal Home </Text></Text>
+      </View>
+ 
+      <Text className='text-[#666876]'>Login to Real Scout with Google</Text>
+
+      <TouchableOpacity
+      activeOpacity={0.7}
+      className= {nativewind.button}
+      onPress={move}
+      >
+
+        <View
+        className=' flex-row justify-center items-center gap-3'
+        >
+
+          <Image source={icons.google}
+          className=' size-7'
+          />
         <Text>Sign Up with Google</Text>
+
+        </View>
       </TouchableOpacity>
+
+      </View>
+      </LinearGradient>
+
     </SafeAreaView>
   )
 }
@@ -36,9 +84,11 @@ const Onboarding = () => {
 export default Onboarding
 
 const styles = StyleSheet.create({
-  screen:{
-    flex: 1,
-    // justifyContent:'center',
-    alignItems:'center'
-  }
+
 })
+
+const nativewind = {
+  screen: 'flex-1  bg-[#f4f4f4]   ',
+// pl-4 pr-4 p-4 
+button:' mt-5 bg-white h-12  w-[85%]  rounded-2xl  justify-center items-center'
+};
